24731ba5c427e7818a660f52a485fa4add81a858
Rosy Lo <rosylo@Rosys-MacBook-Pro.local>
2022-05-21 17:54:11 +0800

    add clicked color

--- src/numberboxList.jsx
+++ src/numberboxList.jsx
@@ -6,11 +6,12 @@ const NumberBoxList = ({ section, numbers, setNumber, state }) => {
                 if(number !== 'sections') {
                     return (
                         <button
+                            className={state[number].includes(section) ? 'clicked' : 'non'} 
                             key={`${section}-${number}`}
                             value={number}
                             name={section}
                             disabled={state[number].includes(section) || !state[number].length ? false : true }
-                            onClick={(e) => {setNumber(e.target.value, e.target.name, false)}}
+                            onClick={(e) => {setNumber(e.target.value, e.target.name)}}
                         >{number}</button>
                     )
                 }
